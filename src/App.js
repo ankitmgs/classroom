49969e39500faad2a69bf3eb38274b450b0be43c
ankitmgs <guptankit753@gmail.com>
2022-01-14 00:00:41 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Header from './components/nav';
+import Home from './components/home';
+import Login from './components/login';
+import Signup from './components/signup';
+import Assignment from './components/assignment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route element = {<Home />} path = "/home" />
+        <Route element = {<Login />} path = "/login" />
+        <Route element = {<Signup />} path = "/signup" />
+        <Route element = {<Assignment />} path = "/assignment" />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/assignment.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Container} from '@mui/material'
+import AssignmentIcon from '@mui/icons-material/Assignment';
+const Assignment = () => {
+    return (
+        <div>
+            <Container>
+            <div className='card'>
+            <AssignmentIcon fontSize='large' id="icon"/>
+            <h3>Assignment 1</h3>
+            </div>
+            
+            </Container>
+        </div>
+    )
+}
+
+export default Assignment;
