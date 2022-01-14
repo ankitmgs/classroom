15ac5ec036c720117d099cd4e754581004bede9c
ankitmgs <guptankit753@gmail.com>
2022-01-15 00:49:31 +0530

    .....

--- src/components/assignment.js
+++ src/components/assignment.js
@@ -1,13 +1,43 @@
 import React from 'react'
-import {Container} from '@mui/material'
+import {Button, Card, CardActions, CardContent, Container, Grid, Typography} from '@mui/material'
 import AssignmentIcon from '@mui/icons-material/Assignment';
 const Assignment = () => {
     return (
         <div>
             <Container>
             <div className='card'>
-            <AssignmentIcon fontSize='large' id="icon"/>
-            <h3>Assignment 1</h3>
+                <div>
+                    <AssignmentIcon fontSize='large' id="icon"/>
+                    <h3 className='assignment_1'>Assignment 1</h3>
+                </div>
+                <div>
+                    <p className='teacher_name'>Teacher Name | Date of Assignment</p>
+                </div>
+                <hr className='hr_line'/>
+                    <div>
+                    <Card className="details-card">
+                        <CardContent>
+                            <Typography variant='h5'>Details</Typography>
+                            <Typography variant='subtitle'>
+                                This is the details section of given assignment.
+                                This is the details section of given assignment.
+                                This is the details section of given assignment.
+                                This is the details section of given assignment.
+                                This is the details section of given assignment.
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button>Read More</Button>
+                        </CardActions>
+                    </Card>
+                    </div>
+                    <div>
+                    <Card className="submit-card">
+                        <CardContent>
+                            <Typography>hello</Typography>
+                        </CardContent>
+                    </Card>
+                    </div>
             </div>
             
             </Container>
